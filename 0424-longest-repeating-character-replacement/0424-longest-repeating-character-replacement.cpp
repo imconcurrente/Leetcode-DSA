d0b026597c71da11f1c22e5a1c538bd560777266
VEDANT B <mevedantbhojne@gmail.com>
2026-07-28 22:20:09 +0530

    Time: 11 ms (29.68%), Space: 11 MB (19.53%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int l = 0, r = 0, maxfreq = 0, maxlen = 0;
+        map<char, int> mpp;
+
+        while (r < n) {
+            mpp[s[r]]++;
+
+            maxfreq = max(maxfreq, mpp[s[r]]);
+
+            while ((r - l + 1) - maxfreq > k) {
+                mpp[s[l]]--;
+                maxfreq = 0;
+
+                for (auto x : mpp) {
+                    maxfreq = max(maxfreq, x.second);
+                }
+                l++;
+            }
+             if ((r - l + 1) - maxfreq <= k) {
+                    maxlen = max(maxlen, (r - l + 1));
+                }
+                r++;
+        }
+        return maxlen;
+    }
+};
