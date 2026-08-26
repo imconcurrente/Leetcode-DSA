67261e1df666ce00da5d4ff6994b29ecb5bcfa43
VEDANT B <mevedantbhojne@gmail.com>
2026-08-27 00:54:08 +0530

    Time: 0 ms (100%), Space: 8.6 MB (95.97%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.length();
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            int ones = 0;
+            for (int j = i; j < n; j++) {
+                if (s[j] == '1') {
+                    ones++;
+                }
+                if (ones == k) {
+                    string curr = s.substr(i, j - i + 1);
+                    if (ans == "") {
+                        ans = curr;
+                    } else if (curr.length() < ans.length()) {
+                        ans = curr;
+                    } else if (curr.length() == ans.length() && curr < ans) {
+                        ans = curr;
+                    }
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
