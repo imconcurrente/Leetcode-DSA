a4b5353e2a305715552947dd11f838b137de891c
VEDANT B <mevedantbhojne@gmail.com>
2026-09-03 23:34:29 +0530

    Time: 0 ms (100%), Space: 165.8 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mini = *min_element(nums1.begin(), nums1.end());
+
+        if (mini % 2 != 0)
+            return true; // minimum is odd
+
+        for (int num : nums1) {
+            if (num % 2 != 0)
+                return false;
+        }
+        return true;
+    }
+};
