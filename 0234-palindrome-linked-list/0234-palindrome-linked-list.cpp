0e585f5721d5b01198787ca5492bd8b31d36e693
VEDANT B <mevedantbhojne@gmail.com>
2026-09-03 16:41:33 +0530

    Time: 6 ms (29.7%), Space: 124.2 MB (34.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,30 +1,61 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+
+    ListNode* reverse(ListNode* head) {
+
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* newHead = reverse(head->next);
+
+        ListNode* front = head->next;
+
+        front->next = head;
+        head->next = NULL;
+
+        return newHead;
+    }
+
     bool isPalindrome(ListNode* head) {
-        stack<int> st;
-        ListNode* temp = head;
 
-        while(temp != NULL){
-            st.push(temp->val);
-            temp =  temp->next;
+        if(head == NULL || head->next == NULL)
+            return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // Find middle
+        while(fast->next != NULL && fast->next->next != NULL) {
+
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        temp = head;
-        while(temp != NULL){
-            if(temp->val != st.top()) return false;
-            temp = temp->next;
-            st.pop();
+        // Reverse second half
+        ListNode* newHead = reverse(slow->next);
+
+        // Connect slow to reversed half
+        slow->next = newHead;
+
+        ListNode* first = head;
+        ListNode* second = newHead;
+
+        // Compare both halves
+        while(second != NULL) {
+
+            if(first->val != second->val) {
+
+                slow->next = reverse(newHead);
+                return false;
+            }
+
+            first = first->next;
+            second = second->next;
         }
+
+        // Restore original list
+        slow->next = reverse(newHead);
+
         return true;
     }
 };
